83a50ae30c4d5a54b50f4cc920923689d0157cc4
April Jay Malaguia <jaymalaguia@gmail.com>
2023-07-18 18:40:57 +0800

    adding Footer.jsx in component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,51 @@
+import Navbar from './component/Navbar.jsx'
+import StudentCard from './component/Footer.jsx'
+
+
 function App() {
+  const students=[
+    {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe',
+      grade: 74
+    },
+    {
+      id: 2,
+      firstName: 'Juan',
+      lastName: 'Dela Cruz',
+      grade: 72
+    },
+    {
+      id: 3,
+      firstName: 'Juan',
+      lastName: ' Cruz',
+      grade: 99
+    },
+    {
+      id: 4,
+      firstName: 'Juan',
+      lastName: 'Dela',
+      grade: 98
+    }
+  ]
+
+  const studentCardList= students.map(
+  (student)=> <StudentCard key={student.id}firstName={student.firstName}lastName={student.lastName}grade={student.grade}/>
+  );
+ 
   return (
     <>
-      <h1>Hello World</h1>
+    <Navbar/>
+
+    {studentCardList}
+
+    <h1 className="text-danger">Hello World!</h1>
+    <i className="fa-solid fa-user"></i>
+    <p className="animate__animated animate__bounce">This is a React component</p>
+    <footer/>
     </>
-  )
+    )
 }
 
 export default App
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,24 @@
+function Navbar(){
+    return (
+        <>
+        <nav className="navbar bg-light">
+            <div className="container">
+                <span className="navbar-brand mb-0 h1">Home</span>
+                <span className="navbar-brand mb-0 h1">About</span>
+                <span className="navbar-brand mb-0 h1">Contact Us</span>
+                <span className="navbar-brand mb-0 h1">Testimonials</span>
+            </div>
+        </nav>
+        <main>
+            <section>
+                main-section
+            </section>
+        </main>
+        <footer>
+            <h1>Footer</h1>
+        </footer>
+        </>
+    )
+}
+
+export default Navbar
